a06b30b7d832baf796589f2027d26332e82ab266
Aniket Handa <atneik@gmail.com>
2016-02-19 00:50:30 -0800

    fixing clear bug

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,18 +10,24 @@ function makeRequest (method, url, done) {
 	xhr.send();
 }
 
+function clearNodeElem(nodeElem) {
+	while (nodeElem.hasChildNodes()) {
+    	nodeElem.removeChild(nodeElem.firstChild);
+	}
+}
+
 function getDiagnosisResults () {	
 	var query = document.getElementById('inputBox').value;
 	var outputElem = document.getElementById('output');
-	while (outputElem.hasChildNodes()) {
-    	outputElem.removeChild(outputElem.firstChild);
-	}
 	if(query.length > 0) {
 		makeRequest('GET', '/search?q=' + query, function (err, data) {
 		  if (err) { throw err; }
+		  clearNodeElem(outputElem);
 		  JSON.parse(data).forEach(function(elem){
 		  	outputElem.innerHTML += '<br/><a href="http://en.wikipedia.org/wiki/'+ elem.str +'">' + elem.str + "</a>" + '<span style="color:rgb(200,200,200)"> - ' + elem.score + "</span>";
 		  });  
 		});
+	} else {
+		clearNodeElem(outputElem);
 	}
 }
